7679cb11fc9d7a38e9b1e1f443a243552b67c0a4
Neil <neil1000404@apps.ntpc.edu.tw>
2024-12-15 00:34:21 +0800

    Add files via upload

---
+++ aaa.js
@@ -0,0 +1,34 @@
+document.getElementById('Economics').addEventListener('click', function() {
+    var content = document.getElementById('eco');
+    
+    // economoy
+    content.style.display = (content.style.display === 'none') ? 'block' : 'none';
+  });
+  document.getElementById('Enviroment').addEventListener('click', function() {
+    var content = document.getElementById('env');
+    
+    // enviroment
+    content.style.display = (content.style.display === 'none') ? 'block' : 'none';
+  });
+  document.getElementById('Society').addEventListener('click', function() {
+    var content = document.getElementById('soc');
+    
+    // sociwrt
+    content.style.display = (content.style.display === 'none') ? 'block' : 'none';
+  });
+  document.getElementById('Phschology').addEventListener('click', function() {
+    var content = document.getElementById('phs');
+    
+    // phschology
+    content.style.display = (content.style.display === 'none') ? 'block' : 'none';
+  });
+    // Fatalities
+  document.getElementById('Fatalities').addEventListener('click', function() {
+    var content = document.getElementById('Fat');
+  content.style.display = (content.style.display === 'none') ? 'block' : 'none';
+  });
+  document.getElementById('Warbenimp').addEventListener('click', function() {
+    var content = document.getElementById('wbi');
+
+    content.style.display = (content.style.display === 'none') ? 'block' : 'none';
+  });
